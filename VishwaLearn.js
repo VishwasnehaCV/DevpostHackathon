dbe10497cfdf63ff7d8cf87aa3c13000afff85ce
Vishwasneha C V <vishwasnehacv@gmail.com>
2024-09-30 22:45:23 +0530

    Add files via upload

---
+++ VishwaLearn.js
@@ -0,0 +1,6 @@
+// Basic form validation
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! We will get back to you soon.');
+  });
+  
